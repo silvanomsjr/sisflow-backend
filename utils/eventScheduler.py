9d7be0fc15127b8558cf666d402e1ea4f962ddf9
VCalixtoR <viniciuscalixto.prof@gmail.com>
2023-07-11 17:50:33 -0300

    feat: changes for async thread scheduler

--- utils/eventScheduler.py
+++ utils/eventScheduler.py
@@ -1,16 +1,33 @@
 import sched, time
+from threading import Thread
 
 class EventScheduler:
 
-  # Creates an event scheduler object using:
-  #   time.monotonic as the time function to measure time intervals
-  #   time.sleep as the delay function compatible with time function to suspend thread execution
-  #
-  #   time.sleep: Uses OSs schedulling syscalls to suspend execution of the current thread
-  #   time.monotonic: Uses OSs clock syscalls to create a monotonic clock based on what time the system is on
   def __init__(self):
+    # Creates an event scheduler object using:
+    #   time.monotonic as the time function to measure time intervals
+    #   time.sleep as the delay function compatible with time function to suspend thread execution
+    #
+    #   time.sleep: Uses OSs schedulling syscalls to suspend execution of the current thread
+    #   time.monotonic: Uses OSs clock syscalls to create a monotonic clock based on what time the system is on
+    #   python scheduler class can be safely used in multi-threaded environments
     self.scheduler = sched.scheduler(timefunc=time.monotonic, delayfunc=time.sleep)
+
+    # Start thread to run scheduler and not block main thread, self is shared
+    self.finishThread = False
+    Thread(target = self.__run).start()
+
+  # Private method used by thread to run scheduler asynchronous
+  def __run(self):
+    print("# Event Scheduler thread created and running!")
+    while self.finishThread == False:
+      self.scheduler.run(blocking=False)
+      time.sleep(1)
   
+  # Stops thread
+  def stop(self):
+    self.finishThread = True
+
   # Enters a event in queue using:
   #   delay in seconds
   #   action function that takes arguments kwargs
@@ -18,11 +35,7 @@ class EventScheduler:
   #   priority with default value 1
   def enterEvent(self, delay, action, kwargs, priority=1):
     self.scheduler.enter(delay, priority, action, kwargs=kwargs)
-  
-  # Send pending queue events by running the event schedule without blocking
-  def sendPendingEvents(self):
-    self.scheduler.run(blocking=False)
-  
+    
   # prints (time, priority, action, argument, kwargs) of each queue object 
   def printEventQueue(self):
     print(self.scheduler.queue)
@@ -48,20 +61,14 @@ def test():
 
   eventScheduler.enterEvent(2, printMonotomicTime, {'parameter': 'parametro 2'})
   eventScheduler.enterEvent(2, printMonotomicTime, {'parameter': 'parametro 2'})
-  
-  print('\n\n\n')
-  eventScheduler.printEventQueue()
-  print('\n\n\n')
 
-  # there is a problem here to solve, events added after start does not changes
   eventScheduler.enterEvent(5, printMonotomicTime, {'parameter': 'parametro 5 added after start'})
   eventScheduler.enterEvent(15, printMonotomicTime, {'parameter': 'parametro 15 added after start'})
 
-  actualTime = time.monotonic()
-  while time.monotonic() < actualTime+20.0:
-    time.sleep(1)
-    eventScheduler.sendPendingEvents()
+  time.sleep(20)
+  
+  eventScheduler.enterEvent(1, printMonotomicTime, {'parameter': 'parametro 21'})
 
-  print('\n\n\n')
-  eventScheduler.printEventQueue()
-  print('\n\n\n')
+  time.sleep(2)
+
+  eventScheduler.stop()
