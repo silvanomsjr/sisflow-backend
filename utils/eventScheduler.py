e0c673ef948474fe31aff5cb6ff78622cc06ba79
VCalixtoR <viniciuscalixto.prof@gmail.com>
2023-07-20 20:11:36 -0300

    feat: remove event from event scheduler

--- utils/eventScheduler.py
+++ utils/eventScheduler.py
@@ -46,8 +46,8 @@ def addTransitionToEventScheduler(eventId, sendDatetime, userHasStateId, userDat
   if systemEventScheduler == None:
     systemEventScheduler = EventScheduler()
 
-  userHasStateId, userData, transitionId
   kwargs = {
+    'eventId': eventId,
     'userHasStateId': userHasStateId,
     'userData': userData,
     'transitionId': transitionId
@@ -55,6 +55,15 @@ def addTransitionToEventScheduler(eventId, sendDatetime, userHasStateId, userDat
 
   delay = (sendDatetime - datetime.now()).total_seconds()
   systemEventScheduler.enterEvent(eventId, delay, priority, action, kwargs)
+
+def removeEventFromScheduler(eventId):
+
+  global systemEventScheduler
+
+  if systemEventScheduler == None:
+    systemEventScheduler = EventScheduler()
+
+  systemEventScheduler.removeEvent(eventId)
 
 def printEventSchedulerInfo():
 
@@ -81,7 +90,7 @@ class EventScheduler:
     #   time.monotonic: Uses OSs clock syscalls to create a monotonic clock based on what time the system is on
     #   python scheduler class can be safely used in multi-threaded environments
     self.scheduler = sched.scheduler(timefunc=time.monotonic, delayfunc=time.sleep)
-    self.schedulerEventIds = []
+    self.schedulerEvents = {}
 
     # Start thread to run scheduler and not block main thread, self is shared
     self.finishThread = False
@@ -93,21 +102,38 @@ class EventScheduler:
     while self.finishThread == False:
       self.scheduler.run(blocking=False)
       time.sleep(1)
-  
+
   # Stops thread
   def stop(self):
     self.finishThread = True
 
   # Enters a event in queue using:
+  #   event id - ignores repeated ids
   #   delay in seconds
   #   action function that takes arguments kwargs
   #   dictionary arguments kwargs
   #   priority with default value 1
   def enterEvent(self, eventId, delay, priority, action, kwargs):
-    self.schedulerEventIds.append(eventId);
-    self.scheduler.enter(delay, priority, action, kwargs=kwargs)
-    
+    if not self.schedulerEvents.get(eventId):
+      event = self.scheduler.enter(delay, priority, action, kwargs=kwargs)
+      self.schedulerEvents[eventId] = event
+    else:
+      print(f'# EventScheduler thread {threading.get_ident()}: Repeated event id ignorated!')
+  
+  # Removes a event in queue using:
+  #   event id
+  def removeEvent(self, eventId):
+    if self.schedulerEvents.get(eventId):
+      
+      # avoid finished event remotion exception
+      try:
+        self.scheduler.cancel(self.schedulerEvents[eventId])
+      except ValueError:
+        pass
+      
+      del self.schedulerEvents[eventId]
+
   # prints event id list and (time, priority, action, argument, kwargs) of each queue object
   def printEvents(self):
-    print(self.schedulerEventIds)
+    print(self.schedulerEvents)
     print(self.scheduler.queue)
